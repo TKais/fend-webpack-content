8adddae534c45df3213cdb78ccdcaab2e69059bf
Tiffany Kaiser <TiffanyTKaiser@gmail.com>
2020-04-19 06:10:00 -0700

    add dev server and linting

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,14 +1,18 @@
-function handleSubmit(event) {
-    event.preventDefault()
+import checkForName from './nameChecker';
 
-    // check what text was put into the form field
-    let formText = document.getElementById('name').value
-    checkForName(formText)
+function _updateUI(data) {
+  document.getElementById('results').innerHTML = data.message;
+}
 
-    console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
+export default function handleSubmit(event) {
+  event.preventDefault();
+
+  // check what text was put into the form field
+  const formText = document.getElementById('name').value;
+  checkForName(formText);
+
+  console.log('::: Form Submitted :::');
+  fetch('http://localhost:8080/test')
     .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
-    })
+    .then(res => _updateUI(res));
 }
---
+++ webpack.dev.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/client/index.js',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'eslint-loader'],
+        },
+      ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        })
+    ]
+  };
